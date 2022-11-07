72ee1d8ff4e0c8f7c8ee182c66401e4dcd9c9b5a
Yurun Ying <yurunying.sh@gmail.com>
2022-11-07 11:25:10 +0000

    modified the combine_raw.R to accomodate the data for sensitivity analyses, close #25

--- Makefile
+++ Makefile
@@ -14,7 +14,18 @@ data/raw/likelihoodD_standardD_Ho-table.csv\
 data/raw/likelihoodD_standardS_Ho-table.csv\
 data/raw/likelihoodS_standardD_Ho-table.csv\
 data/raw/likelihoodS_standardS_Ho-table.csv
-	$^
+	$^ $@
+	
+data/processed/sa_casual_sex_sim.csv : code/combine_raw.R\
+data/raw/sa_likelihoodD_standardD_He-table.csv\
+data/raw/sa_likelihoodD_standardS_He-table.csv\
+data/raw/sa_likelihoodS_standardD_He-table.csv\
+data/raw/sa_likelihoodS_standardS_He-table.csv\
+data/raw/sa_likelihoodD_standardD_Ho-table.csv\
+data/raw/sa_likelihoodD_standardS_Ho-table.csv\
+data/raw/sa_likelihoodS_standardD_Ho-table.csv\
+data/raw/sa_likelihoodS_standardS_Ho-table.csv
+	$^ $@
 
 data/processed/des_and_test_result.csv : code/des_and_test_result.R\
 data/processed/casual_sex_sim.csv
--- code/combine_raw.R
+++ code/combine_raw.R
@@ -1,15 +1,42 @@
 #!/usr/bin/env Rscript --vanilla
 
 # name: combine_raw.R
-# input: eight dataset by conditions from the two experiments
-# output: one dataset with coded conditions and preprocessed outcomes
+# input: eight dataset by conditions from the two experiments, for either main analyses or sensitivity analyses
+# output: one dataset with coded conditions and preprocessed outcomes, for either main analyses or sensitivity analyses
 # notes: conditions were coded into variables diff_likelihood, diff_standard, and homosexual
 
-library(tidyverse); library(magrittr)
+library(tidyverse)
 
-input_file <- commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
-output_file <- "data/processed/casual_sex_sim.csv"
+input_file <- args[1:8]
+# # test for main analyses
+# input_file <- c("data/raw/likelihoodD_standardD_He-table.csv",
+#                 "data/raw/likelihoodD_standardS_He-table.csv",
+#                 "data/raw/likelihoodS_standardD_He-table.csv",
+#                 "data/raw/likelihoodS_standardS_He-table.csv",
+#                 "data/raw/likelihoodD_standardD_Ho-table.csv",
+#                 "data/raw/likelihoodD_standardS_Ho-table.csv",
+#                 "data/raw/likelihoodS_standardD_Ho-table.csv",
+#                 "data/raw/likelihoodS_standardS_Ho-table.csv")
+# 
+# # test for sensitivity analyses
+# input_file <- c("data/raw/sa_likelihoodD_standardD_He-table.csv",
+#                 "data/raw/sa_likelihoodD_standardS_He-table.csv",
+#                 "data/raw/sa_likelihoodS_standardD_He-table.csv",
+#                 "data/raw/sa_likelihoodS_standardS_He-table.csv",
+#                 "data/raw/sa_likelihoodD_standardD_Ho-table.csv",
+#                 "data/raw/sa_likelihoodD_standardS_Ho-table.csv",
+#                 "data/raw/sa_likelihoodS_standardD_Ho-table.csv",
+#                 "data/raw/sa_likelihoodS_standardS_Ho-table.csv")
+
+output_file <- args[9]
+
+# # test for main analyses
+# output_file <- "data/processed/casual_sex_sim.csv"
+# 
+# # test for main analyses
+# output_file <- "data/processed/sa_casual_sex_sim.csv"
 
 # read file and combine
 raw_tib <- map_dfr(.x = input_file, .f = read_csv, skip = 6)
@@ -17,10 +44,10 @@ raw_tib <- map_dfr(.x = input_file, .f = read_csv, skip = 6)
 # remove unnecessary columns and rename the variables
 raw_tib <- 
   raw_tib %>% 
-  select(-c("[run number]", "movement-range", "long-term-likelihood",
-              "[step]"))
+  select(-c("[run number]", "movement-range", "[step]"))
 
-names(raw_tib) <- c("m_number", "m_likelihood", "f_likelihood",
+names(raw_tib) <- c("m_number", "lt_likelihood", 
+                    "m_likelihood", "f_likelihood",
                     "m_standard", "f_standard", "homosexual",
                     "seed", "m_inpool", "f_inpool",
                     "m_exp", "f_exp", "m_partner", "f_partner")
@@ -52,7 +79,8 @@ raw_tib <-
     relocate(seed, .before = everything()) %>% 
     relocate(homosexual, .after = diff_standard) %>% 
     relocate(m_inpool, .before = m_outpool) %>% 
-    relocate(f_inpool, .after = m_inpool)
+    relocate(f_inpool, .after = m_inpool) %>% 
+    relocate(lt_likelihood, .after = everything())
 
 # output
 write_csv(raw_tib, output_file)
